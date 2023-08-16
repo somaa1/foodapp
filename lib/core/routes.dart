fb548d429c06dc8abe056340ea92766cf763e029
somaa1 <ismailrabie111@gmail.com>
2023-08-16 12:21:55 +0300

    Initial commit

---
+++ lib/core/routes.dart
@@ -0,0 +1,10 @@
+class Routes {
+  static const String splashScreen = '/splash_screen';
+  static const String aboutScreen = '/about_screen';
+  static const String outBoardingScreen = '/out_boarding_screen';
+
+  static const String authenticationView = '/authenticationView';
+  static const String loginView = '/loginView';
+  static const String registerView = '/registerView';
+  static const String homeView = '/HomeView';
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'core/routes.dart';
+import 'features/out_boarding/presentation/view/out_boarding_screen.dart';
+import 'features/splash/presentation/view/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.splashScreen,
+      routes: {
+        Routes.splashScreen: (context) => const SplashScreen(),
+        Routes.outBoardingScreen: (context) => const OutBoardingScreen(),
+        // Routes.authenticationView: (context) => const AuthenticationView(),
+        // Routes.loginView: (context) => const LoginView(),
+        // Routes.registerView: (context) => const RegisterView(),
+        // Routes.homeView: (context) => const HomeView(),
+      },
+    );
+  }
+}
