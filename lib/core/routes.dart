27c9afb4188a29c9cf8fe8c9004fd3b0ae44b0bb
somaa1 <ismailrabie111@gmail.com>
2023-08-26 13:28:08 +0300

    create class RouteGenerator

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:foodapp/core/resources/manager_strings.dart';
+
+import '../features/auth/presentation/view/Login_Page.dart';
+import '../features/auth/presentation/view/Regiester_Page.dart';
+import '../features/home/presentation/view/home_view.dart';
+import '../features/out_boarding/presentation/view/out_boarding_screen.dart';
+import '../features/splash/presentation/view/splash_screen.dart';
+
 class Routes {
   static const String splashScreen = '/splash_screen';
   static const String aboutScreen = '/about_screen';
@@ -7,4 +17,34 @@ class Routes {
   static const String loginView = '/loginView';
   static const String registerView = '/registerView';
   static const String homeView = '/HomeView';
+}
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashScreen:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case Routes.outBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OutBoardingScreen());
+
+      case Routes.loginView:
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      case Routes.registerView:
+        return MaterialPageRoute(builder: (_) => const RegiesterPage());
+      case Routes.homeView:
+        return MaterialPageRoute(builder: (_) => const HomeView());
+      default:
+        return unDefineRoute();
+    }
+  }
+
+  static Route<dynamic> unDefineRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text(ManagerStrings.notFoundRoute),
+        ),
+      ),
+    );
+  }
 }
