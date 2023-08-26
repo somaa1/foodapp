31ea1812bc582c4df7744dc2fb4bd8e4835663ab
somaa1 <ismailrabie111@gmail.com>
2023-08-26 13:29:31 +0300

    add some assets

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -2,20 +2,34 @@
 class Assets {
   Assets._();
 
-  static const String authAuthBg = 'assets/images/auth/auth_bg.png';
-  static const String authFacebook = 'assets/images/auth/facebook.png';
-  static const String authGoogle = 'assets/images/auth/google.png';
-  static const String authLoginLogo = 'assets/images/auth/login_logo.png';
-  static const String authLogo = 'assets/images/auth/logo.png';
-  static const String authTwitter = 'assets/images/auth/twitter.png';
-  static const String cairoCairoBold = 'assets/fonts/cairo/Cairo-Bold.ttf';
-  static const String cairoCairoRegular = 'assets/fonts/cairo/Cairo-Regular.ttf';
+  static const String assetsHomeImage = 'assets/home_image.png';
+  static const String authAuth2 = 'assets/images/auth/auth2.png';
+  static const String authAuthh1 = 'assets/images/auth/authh1.png';
+  static const String homeHomeImage = 'assets/images/home/home_image.png';
   static const String outBoardingOutbor1 = 'assets/images/out_boarding/outbor1.png';
   static const String outBoardingOutbor2 = 'assets/images/out_boarding/outbor2.png';
   static const String outBoardingOutbor3 = 'assets/images/out_boarding/outbor3.png';
+  static const String outBoardingOutbor33 = 'assets/images/out_boarding/outbor33.jpg';
+  static const String poppinsPoppinsBlack = 'assets/fonts/Poppins/Poppins-Black.ttf';
+  static const String poppinsPoppinsBlackItalic = 'assets/fonts/Poppins/Poppins-BlackItalic.ttf';
+  static const String poppinsPoppinsBold = 'assets/fonts/Poppins/Poppins-Bold.ttf';
+  static const String poppinsPoppinsBoldItalic = 'assets/fonts/Poppins/Poppins-BoldItalic.ttf';
+  static const String poppinsPoppinsExtraBold = 'assets/fonts/Poppins/Poppins-ExtraBold.ttf';
+  static const String poppinsPoppinsExtraBoldItalic = 'assets/fonts/Poppins/Poppins-ExtraBoldItalic.ttf';
+  static const String poppinsPoppinsExtraLight = 'assets/fonts/Poppins/Poppins-ExtraLight.ttf';
+  static const String poppinsPoppinsExtraLightItalic = 'assets/fonts/Poppins/Poppins-ExtraLightItalic.ttf';
+  static const String poppinsPoppinsItalic = 'assets/fonts/Poppins/Poppins-Italic.ttf';
+  static const String poppinsPoppinsLight = 'assets/fonts/Poppins/Poppins-Light.ttf';
+  static const String poppinsPoppinsLightItalic = 'assets/fonts/Poppins/Poppins-LightItalic.ttf';
+  static const String poppinsPoppinsMedium = 'assets/fonts/Poppins/Poppins-Medium.ttf';
+  static const String poppinsPoppinsMediumItalic = 'assets/fonts/Poppins/Poppins-MediumItalic.ttf';
+  static const String poppinsPoppinsRegular = 'assets/fonts/Poppins/Poppins-Regular.ttf';
+  static const String poppinsPoppinsSemiBold = 'assets/fonts/Poppins/Poppins-SemiBold.ttf';
+  static const String poppinsPoppinsSemiBoldItalic = 'assets/fonts/Poppins/Poppins-SemiBoldItalic.ttf';
+  static const String poppinsPoppinsThin = 'assets/fonts/Poppins/Poppins-Thin.ttf';
+  static const String poppinsPoppinsThinItalic = 'assets/fonts/Poppins/Poppins-ThinItalic.ttf';
   static const String splashLogo = 'assets/images/splash/logo.png';
   static const String splashSplash1 = 'assets/images/splash/splash1.png';
   static const String splashSplash2 = 'assets/images/splash/splash2.png';
-  static const String splashSplash3 = 'assets/images/splash/splash3.png';
 
 }
