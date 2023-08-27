cbeb2ac5c3fb0fcd69a87b6d28c7939345b41851
somaa1 <ismailrabie111@gmail.com>
2023-08-27 19:31:07 +0300

    create class SharedPref

--- lib/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart
+++ lib/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart
@@ -1,1 +1,12 @@
 
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesSetting{
+  static final _instance=SharedPreferencesSetting._internal();
+  late SharedPreferences _sharedPreferences;
+  SharedPreferencesSetting._internal();
+  factory SharedPreferencesSetting() {
+    return _instance;
+  }
+
+}
