5b34d884601495caeb5b3eb6d5260ac92c1b7f35
somaa1 <ismailrabie111@gmail.com>
2023-08-27 20:18:00 +0300

    put initSplash and  disposeSplash in dependancy_injection

--- lib/core/config/dependancy_injection.dart
+++ lib/core/config/dependancy_injection.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:foodapp/core/storage/local/database/shared_preferences/app_settings_shared_preferences.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../features/splash/controller/splash_controller.dart';
 
 initModule() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferencesSetting().initPreferences();
+}
+initSplash() {
+  Get.put<SplashController>(SplashController());
+}
+
+disposeSplash() {
+  Get.delete<SplashController>();
 }
